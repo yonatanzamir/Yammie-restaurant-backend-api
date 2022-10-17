3332e94d694fe10228789f618dd0fdc5fe8c48a2
Yonatan Zamir <65045014+yonatanzamir@users.noreply.github.com>
2022-10-17 18:39:25 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const dotenv = require("dotenv")
 dotenv.config()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, function () {
     console.log(`app is listening on port ${port}`);
 });
